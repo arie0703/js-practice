48c0aab26c47b9bdd0d6f470b21799ee847c926e
[hitodeface] <[ari0703sophia@gmail.com]>
2021-01-30 00:11:47 +0900

    first commit

---
+++ todo/script.js
@@ -0,0 +1,30 @@
+var app = new Vue ({
+	el: '#app',
+  data: {
+  	newItem: '',
+    todos: [],
+    counter: 0,
+  },
+  methods: {
+  	addItem: function(event) {
+    
+    	if(this.newItem == "") return;
+      
+    	var todo = {
+      	item: this.newItem,
+        isDone: false,
+      };
+      this.todos.push(todo);
+      this.newItem = "";
+    },
+    deleteItem: function(index) {
+    	this.todos.splice(index, 1);
+    }
+  },
+  computed: {
+    //todosを降順に表示させる。
+    desc_todos() {
+      return this.todos.slice().reverse();
+    },
+  }
+})
