23d85ccf96180613f714154c1083da725392d22b
[hitodeface] <[ari0703sophia@gmail.com]>
2021-01-31 16:16:23 +0900

    axios and tabmenu practice

---
+++ app/js/tab-menu.js
@@ -0,0 +1,11 @@
+var tab = new Vue({
+    el: '#tab-menu',
+    data: {
+        isActive: '0',
+        messages: [
+            'this is tab 1.',
+            'this is tab 2.',
+            'this is tab 3.',
+        ],
+    },
+})
